88e083a9b59aa26032fbb9e9d229ddc7dbbea5c5
justomont <justomontoyagalvez@gmail.com>
2026-06-29 11:38:10 +0200

    Rename Autoelectrodes module to SlicerSEEGRecon
    Rename the module and update related resources and metadata: change CMake MODULE_NAME, Python module and class names (Autoelectrodes -> SlicerSEEGRecon) including widget, logic and test classes, update UI and icon filenames and load paths, update sample data identifiers and thumbnails, update module title, category, dependencies, contributors, helpText and acknowledgement, and adjust module selection calls. Binary .DS_Store files and resource renames included.

--- SlicerSEEGRecon/SlicerSEEGRecon.py
+++ SlicerSEEGRecon/SlicerSEEGRecon.py
@@ -969,28 +969,28 @@ def importAndLoadDICOMFolder(dicom_folder):
                 DICOMLib.loadSeriesByUID([seriesUID])
         
 #
-# Autoelectrodes
+# SlicerSEEGRecon
 #
 
-class Autoelectrodes(ScriptedLoadableModule):
+class SlicerSEEGRecon(ScriptedLoadableModule):
     """Uses ScriptedLoadableModule base class, available at:
     https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
     """
 
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
-        self.parent.title = "Autoelectrodes"  # TODO: make this more human readable by adding spaces
-        self.parent.categories = ["SEEG"]  # TODO: set categories (folders where the module shows up in the module selector)
-        self.parent.dependencies = []  # TODO: add here list of module names that this module requires
-        self.parent.contributors = ["Justo Montoya (Pompeu Fabra University)"]  # TODO: replace with "Firstname Lastname (Organization)"
+        self.parent.title = "SlicerSEEGRecon"  
+        self.parent.categories = ["Electrophysiology"]  
+        self.parent.dependencies = ["SlicerFreeSurfer;SlicerElastix"]  
+        self.parent.contributors = ["Justo Montoya-Gálvez (Pompeu Fabra University; UPF); Alessandro Principe (Pompeu Fabra University & Hospital del Mar Research Institute; UPF-IMIM)"] 
         # TODO: update with short description of the module and a link to online module documentation
         self.parent.helpText = """
 This is an example of scripted loadable module bundled in an extension.
-See more information in <a href="https://github.com/organization/projectname#Autoelectrodes">module documentation</a>.
+See more information in <a href="https://github.com/justomont/SlicerSEEGRecon">module documentation</a>.
 """
         # TODO: replace with organization, grant and thanks
         self.parent.acknowledgementText = """
-This file was originally developed by Justo Montoya and was partially funded by XXXXXXX grant XXXXXXXXXXXX.
+This file was originally developed by Justo Montoya-Gálvez & Alessandro Principe and was partially funded by Agència de Gestió d’Ajuts Universitaris i de Recerca (AGAUR) Generalitat de Catalunya grant number FI-SDUR 20203.
 """
 
         # Additional initialization step after application startup is complete
@@ -1013,50 +1013,50 @@ def registerSampleData():
     # To ensure that the source code repository remains small (can be downloaded and installed quickly)
     # it is recommended to store data sets that are larger than a few MB in a Github release.
 
-    # Autoelectrodes1
+    # SlicerSEEGRecon1
     SampleData.SampleDataLogic.registerCustomSampleDataSource(
         # Category and sample name displayed in Sample Data module
-        category='Autoelectrodes',
-        sampleName='Autoelectrodes1',
+        category='SlicerSEEGRecon',
+        sampleName='SlicerSEEGRecon1',
         # Thumbnail should have size of approximately 260x280 pixels and stored in Resources/Icons folder.
         # It can be created by Screen Capture module, "Capture all views" option enabled, "Number of images" set to "Single".
-        thumbnailFileName=os.path.join(iconsPath, 'Autoelectrodes1.png'),
+        thumbnailFileName=os.path.join(iconsPath, 'SlicerSEEGRecon1.png'),
         # Download URL and target file name
         uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
-        fileNames='Autoelectrodes1.nrrd',
+        fileNames='SlicerSEEGRecon1.nrrd',
         # Checksum to ensure file integrity. Can be computed by this command:
         #  import hashlib; print(hashlib.sha256(open(filename, "rb").read()).hexdigest())
         checksums='SHA256:998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95',
         # This node name will be used when the data set is loaded
-        nodeNames='Autoelectrodes1'
+        nodeNames='SlicerSEEGRecon1'
     )
 
-    # Autoelectrodes2
+    # SlicerSEEGRecon2
     SampleData.SampleDataLogic.registerCustomSampleDataSource(
         # Category and sample name displayed in Sample Data module
-        category='Autoelectrodes',
-        sampleName='Autoelectrodes2',
-        thumbnailFileName=os.path.join(iconsPath, 'Autoelectrodes2.png'),
+        category='SlicerSEEGRecon',
+        sampleName='SlicerSEEGRecon2',
+        thumbnailFileName=os.path.join(iconsPath, 'SlicerSEEGRecon2.png'),
         # Download URL and target file name
         uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
-        fileNames='Autoelectrodes2.nrrd',
+        fileNames='SlicerSEEGRecon2.nrrd',
         checksums='SHA256:1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97',
         # This node name will be used when the data set is loaded
-        nodeNames='Autoelectrodes2'
+        nodeNames='SlicerSEEGRecon2'
     )
 
 #
-# AutoelectrodesWidget
+# SlicerSEEGReconWidget
 #
 
-class AutoelectrodesWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
+class SlicerSEEGReconWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
     """Uses ScriptedLoadableModuleWidget base class, available at:
     https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
     """
 
     def __init__(self, parent=None):
         """Traceback (most recent call last):
-  File "/home/marzieh/Dropbox/M/my-plotly/SlicerAutoelectrodes-main/SlicerAutoelectrodes/Autoelectrodes/Autoelectrodes.py", line 1636, in openEDFviewer
+  File "/home/marzieh/Dropbox/M/my-plotly/SlicerSlicerSEEGRecon-main/SlicerSlicerSEEGRecon/SlicerSEEGRecon/SlicerSEEGRecon.py", line 1636, in openEDFviewer
     popup = PlotWindow()
 NameError: name 'PlotWindow' is not defined
         Called when the user opens the module the first time and the widget is initialized.
@@ -1075,7 +1075,7 @@ NameError: name 'PlotWindow' is not defined
 
         # Load widget from .ui file (created by Qt Designer).
         # Additional widgets can be instantiated manually and added to self.layout.
-        uiWidget = slicer.util.loadUI(self.resourcePath('UI/Autoelectrodes.ui'))
+        uiWidget = slicer.util.loadUI(self.resourcePath('UI/SlicerSEEGRecon.ui'))
         self.layout.addWidget(uiWidget)
         # self.ui = slicer.util.childWidgetVariables(uiWidget)
         
@@ -1108,7 +1108,7 @@ NameError: name 'PlotWindow' is not defined
 
         # Create logic class. Logic implements all computations that should be possible to run
         # in batch mode, without a graphical user interface.
-        self.logic = AutoelectrodesLogic()
+        self.logic = SlicerSEEGReconLogic()
 
         # Initialize parameter node first
         self.initializeParameterNode()
@@ -1415,10 +1415,10 @@ NameError: name 'PlotWindow' is not defined
         
 
 #
-# AutoelectrodesLogic
+# SlicerSEEGReconLogic
 #
 
-class AutoelectrodesLogic(ScriptedLoadableModuleLogic):
+class SlicerSEEGReconLogic(ScriptedLoadableModuleLogic):
     """This class should implement all the actual
     computation done by your module.  The interface
     should be such that other python code can import
@@ -1842,7 +1842,7 @@ class AutoelectrodesLogic(ScriptedLoadableModuleLogic):
         ctFinalVolumeNode.SetAndObserveTransformNodeID(CTtoGoal_Transform.GetID())
         slicer.vtkSlicerTransformLogic().hardenTransform(ctFinalVolumeNode)
 
-        slicer.util.selectModule("Autoelectrodes")
+        slicer.util.selectModule("SlicerSEEGRecon")
 
         print("     Done!")
 
@@ -2131,10 +2131,10 @@ class AutoelectrodesLogic(ScriptedLoadableModuleLogic):
             
 
 #
-# AutoelectrodesTest
+# SlicerSEEGReconTest
 #
 
-class AutoelectrodesTest(ScriptedLoadableModuleTest):
+class SlicerSEEGReconTest(ScriptedLoadableModuleTest):
     """
     This is the test case for your scripted module.
     Uses ScriptedLoadableModuleTest base class, available at:
@@ -2150,9 +2150,9 @@ class AutoelectrodesTest(ScriptedLoadableModuleTest):
         """Run as few or as many tests as needed here.
         """
         self.setUp()
-        self.test_Autoelectrodes1()
+        self.test_SlicerSEEGRecon1()
 
-    def test_Autoelectrodes1(self):
+    def test_SlicerSEEGRecon1(self):
         """ Ideally you should have several levels of tests.  At the lowest level
         tests should exercise the functionality of the logic with different inputs
         (both valid and invalid).  At higher levels your tests should emulate the
@@ -2170,7 +2170,7 @@ class AutoelectrodesTest(ScriptedLoadableModuleTest):
 
         # import SampleData
         # registerSampleData()
-        # inputVolume = SampleData.downloadSample('Autoelectrodes1')
+        # inputVolume = SampleData.downloadSample('SlicerSEEGRecon1')
         
         testPath = '/Volumes/GoogleDrive/Mi unidad/_PhD/SLICER/test_subject_1/real.mrml'
         slicer.util.loadScene(testPath)
@@ -2186,7 +2186,7 @@ class AutoelectrodesTest(ScriptedLoadableModuleTest):
 
         # Test the module logic
 
-        logic = AutoelectrodesLogic()
+        logic = SlicerSEEGReconLogic()
 
         # Test algorithm with non-inverted threshold
         logic.process()
